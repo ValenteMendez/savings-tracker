5498e33b87a3c70d0df01bae70dba5c37806578d
Valente <evmendezs@gmail.com>
2025-01-07 23:57:27 +0100

    Added config file

---
+++ components/ui/alerts.js
@@ -0,0 +1,36 @@
+// components/ui/alert.jsx
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+const Alert = React.forwardRef(({ className, variant, ...props }, ref) => (
+  <div
+    ref={ref}
+    role="alert"
+    className={cn(
+      "relative w-full rounded-lg border px-4 py-3",
+      className
+    )}
+    {...props}
+  />
+))
+Alert.displayName = "Alert"
+
+const AlertTitle = React.forwardRef(({ className, ...props }, ref) => (
+  <h5
+    ref={ref}
+    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+    {...props}
+  />
+))
+AlertTitle.displayName = "AlertTitle"
+
+const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm", className)}
+    {...props}
+  />
+))
+AlertDescription.displayName = "AlertDescription"
+
+export { Alert, AlertTitle, AlertDescription }
